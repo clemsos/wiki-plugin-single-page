2db98ec330b428e3cff0bd3f6d088ce763fd130f
Clément Renaud <clement.renaud@gmail.com>
2016-06-09 17:33:15 +0200

    add basic parsing, json extraction and single URL home page

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 var express = require('express'),
   exphbs  = require('express-handlebars'),
-  path = require('path');
+  request = require("request"),
+  url = require('url');
+
+var helpers = require("./helpers")
 
 var port = process.env.PORT || 4000;
 var rootDir = __dirname;
 
+// handlebars config
+var hbs = exphbs.create({
+  defaultLayout: 'main',
+  helpers : helpers
+});
+
 var app = express();
 
 // express config
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.set('views', rootDir + '/views');
@@ -17,8 +26,55 @@ app.use(express.static('public'));
 // serve skeleton from node_modules
 app.use('/css', express.static(__dirname + '/node_modules/skeleton-css/css/'));
 
+var convertURL = function(completeURL) {
+  var parsed = url.parse(completeURL);
+  console.log(parsed);
+  // TODO: solid url parsing
+  var urls = [];
+  parsed.pathname.split("/view").forEach(function(d){
+    if(d) {
+      var jsonUrl = parsed.protocol + "//" + parsed.host + "/" + d.split("/")[1] + ".json";
+      urls.push(jsonUrl)
+    }
+  })
+
+  return urls
+}
+
+var testURL = "http://david.bovill.me/view/welcome-visitors/view/ushahidi";
+console.log(convertURL(testURL));
+
+function fetchJSON(url, callback) {
+  console.log("fetching %s", url);
+  request(url, function(err, res, body) {
+    if(err) throw err
+    callback (JSON.parse(body))
+  });
+}
+
+
+
 app.get('/', function (req, res) {
-  res.render('home');
+  res.render('home')
+})
+
+app.get('/p/', function (req, res) {
+
+   var url = req.query.url;
+   // TODO validate URI
+
+   var urls = convertURL(url);
+
+   //TODO: pass whole array
+   fetchJSON(urls[urls.length-1], function(data){
+    res.render('parser', {
+      title : data.title,
+      story : data.story,
+      data : data
+    });
+  })
+
+
 });
 
 app.listen(port, function () {
